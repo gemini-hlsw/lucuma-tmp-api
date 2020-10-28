ef1d1ee14e69d87d761db8c5384646189c2c9bcf
Shane Walker <swalker@gemini.edu>
2020-10-28 15:15:56 -0300

    unshare too

--- modules/core/src/main/scala/lucuma/odb/api/schema/SharingMutation.scala
+++ modules/core/src/main/scala/lucuma/odb/api/schema/SharingMutation.scala
@@ -17,6 +17,8 @@ import sangria.marshalling.circe._
 import sangria.schema._
 import monocle.state.all._
 
+import scala.concurrent.Future
+
 
 /**
  *
@@ -63,52 +65,107 @@ trait SharingMutation {
       o <- Tables.retrieveObservation(observationId)
     } yield (a, List[Long => Event](AsterismEvent(Created, a), ObservationEvent(Updated, o)))
 
+  // Produces a `State` computation that adds or removes targets from an
+  // asterism
+  private def updateAsterism(
+    asterismId: AsterismModel.Id,
+    update:     Set[TargetModel.Id] => Set[TargetModel.Id]
+  ): State[Tables, (AsterismModel, List[Long => Event])] =
+    Tables.retrieveAsterism(asterismId).transform { (t, a) =>
+      val newIds = update(a.targetIds)
+      if (newIds == a.targetIds)
+        (t, (a, List.empty[Long => Event]))
+      else {
+        val aʹ = AsterismModel.Default.asterismTargetIds.set(newIds)(a)
+        (Tables.asterisms.modify(_.updated(asterismId, aʹ))(t), (aʹ, List[Long => Event](AsterismEvent(Updated, aʹ))))
+      }
+    }
+
+  /**
+   * Returns a `State[Tables, ?]` program that will update a `Default` asterism,
+   * adding the given target ids.
+   *
+   * @param asterismId asterism to update (assuming it is a `Default` asterism
+   * @param targetIds target ids to _add_
+   *
+   * @return updated `Default` asterism paired with the list of resultant events
+   *         that should be published
+   */
   private def addTargetsToAsterism(
     asterismId: AsterismModel.Id,
     targetIds:  Set[TargetModel.Id]
   ): State[Tables, (AsterismModel, List[Long => Event])] =
-    for {
-      a   <- Tables.retrieveAsterism(asterismId)
-      tup <- Tables.asterism(asterismId).st.transform { (t, _) =>
-        if (a.targetIds == targetIds)
-          (t, (a, List.empty[Long => Event]))
-        else  {
-          val a2 = AsterismModel.Default.asterismTargetIds.modify(_ ++ targetIds)(a)
-          (Tables.asterisms.modify(_.updated(asterismId, a2))(t), (a2, List[Long => Event](AsterismEvent(Updated, a2))))
-        }
-      }
-    } yield tup
+    updateAsterism(asterismId, _ ++ targetIds)
+
+  /**
+   * Returns a `State[Tables, ?]` program that will update a `Default` asterism,
+   * removing the given target ids.
+   *
+   * @param asterismId asterism to update (assuming it is a `Default` asterism
+   * @param targetIds target ids to remove
+   *
+   * @return updated `Default` asterism paired with the list of resultant events
+   *         that should be published
+   */
+  private def removeTargetsFromAsterism(
+    asterismId: AsterismModel.Id,
+    targetIds:  Set[TargetModel.Id]
+  ): State[Tables, (AsterismModel, List[Long => Event])] =
+    updateAsterism(asterismId, _ -- targetIds)
+
+  // Implements the bulk of a share/unshare field for targets and observations.
+  // Takes a function that produces a `State` operation to apply to do the
+  // tables.
+  private def targetObservationShare[F[_]: Effect](
+    c: Context[OdbRepo[F], Unit]
+  )(
+    f: (Set[TargetModel.Id], List[ObservationModel]) => State[Tables, List[(AsterismModel, List[Long => Event])]]
+  ): Future[List[AsterismModel]] = {
+
+    val links   = c.arg(ArgumentTargetObservationLinks)
+
+    val updates = c.ctx.tables.modify { t =>
+      val targetIds  = links.targets.toSet
+      val targetList = links.targets.traverse(LookupSupport.lookupTarget(t, _))
+      val obsList    = links.observations.traverse(LookupSupport.lookupObservation(t, _))
+      (targetList, obsList).mapN { (_, os) =>
+        f(targetIds, os).run(t).value
+      }.fold(err => (t, InputError.Exception(err).asLeft), _.map(_.asRight))
+    }
+
+    (for {
+      us <- EitherT(updates).rethrowT
+      (as, es) = us.unzip
+      _  <- es.flatten.traverse(c.ctx.eventService.publish)
+    } yield as).toIO.unsafeToFuture()
+  }
 
   def shareTargetsWithObservations[F[_]: Effect]: Field[OdbRepo[F], Unit] =
     Field(
       name      = "shareTargetsWithObservations",
       fieldType = ListType(AsterismType[F]),
       arguments = List(ArgumentTargetObservationLinks),
-      resolve   = c => {
-        val links   = c.arg(ArgumentTargetObservationLinks)
-
-        val updates = c.ctx.tables.modify { t =>
-          val targetList = links.targets.traverse(LookupSupport.lookupTarget(t, _))
-          val obsList    = links.observations.traverse(LookupSupport.lookupObservation(t, _))
-          (targetList, obsList).mapN { (_, os) =>
-            os.traverse { o =>
-              val targetIds = links.targets.toSet
-              o.asterismId.fold(createDefaultAsterism(o.id, targetIds))(addTargetsToAsterism(_, targetIds))
-            }.run(t).value
-          }.fold(err => (t, InputError.Exception(err).asLeft), _.map(_.asRight))
+      resolve   = c => targetObservationShare[F](c) { (targetIds, obsList) =>
+        obsList.traverse { o =>
+          o.asterismId.fold(createDefaultAsterism(o.id, targetIds))(addTargetsToAsterism(_, targetIds))
         }
+      }
+    )
 
-        (for {
-          us <- EitherT(updates).rethrowT
-          (as, es) = us.unzip
-          _  <- es.flatten.traverse(c.ctx.eventService.publish)
-        } yield as).toIO.unsafeToFuture()
+  def unshareTargetsWithObservations[F[_]: Effect]: Field[OdbRepo[F], Unit] =
+    Field(
+      name      = "unshareTargetsWithObservations",
+      fieldType = ListType(AsterismType[F]),
+      arguments = List(ArgumentTargetObservationLinks),
+      resolve   = c => targetObservationShare[F](c) { (targetIds, obsList) =>
+        obsList.flatMap(_.asterismId.toList).traverse(removeTargetsFromAsterism(_, targetIds))
       }
     )
 
   def allFields[F[_]: Effect]: List[Field[OdbRepo[F], Unit]] =
     List(
-      shareTargetsWithObservations
+      shareTargetsWithObservations,
+      unshareTargetsWithObservations
     )
 
 }
