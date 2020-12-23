8bd8ddf64de7e33a39308e61d37ea95736a43e7d
Shane Walker <swalker@gemini.edu>
2020-12-23 17:52:06 -0300

    Unset asterism, etc. with null value

--- modules/core/src/main/scala/lucuma/odb/api/model/ObservationModel.scala
+++ modules/core/src/main/scala/lucuma/odb/api/model/ObservationModel.scala
@@ -4,12 +4,18 @@
 package lucuma.odb.api.model
 
 import lucuma.odb.api.model.Existence._
+import lucuma.odb.api.model.syntax.input._
 import lucuma.core.`enum`.ObsStatus
 import lucuma.core.optics.syntax.lens._
 import lucuma.core.model.{Asterism, Observation, Program}
+
 import cats.Eq
 import cats.data.State
-import cats.syntax.validated._
+import cats.syntax.apply._
+import cats.syntax.traverse._
+import clue.data.Input
+import eu.timepit.refined.cats._
+import eu.timepit.refined.types.string._
 import io.circe.Decoder
 import io.circe.generic.semiauto._
 import monocle.Lens
@@ -19,7 +25,7 @@ final case class ObservationModel(
   id:                 Observation.Id,
   existence:          Existence,
   programId:          Program.Id,
-  name:               Option[String],
+  name:               Option[NonEmptyString],
   status:             ObsStatus,
   asterismId:         Option[Asterism.Id],
   plannedTimeSummary: PlannedTimeSummaryModel
@@ -41,16 +47,20 @@ object ObservationModel extends ObservationOptics {
     status:        Option[ObsStatus]
   ) {
 
-    def withId(id: Observation.Id, s: PlannedTimeSummaryModel): ObservationModel =
-      ObservationModel(
-        id,
-        Present,
-        programId,
-        name,
-        status.getOrElse(ObsStatus.New),
-        asterismId,
-        s
-      )
+    def withId(s: PlannedTimeSummaryModel): ValidatedInput[Observation.Id => ObservationModel] =
+      name
+        .traverse(ValidatedInput.nonEmptyString("name", _))
+        .map { n => oid =>
+          ObservationModel(
+            oid,
+            Present,
+            programId,
+            n,
+            status.getOrElse(ObsStatus.New),
+            asterismId,
+            s
+          )
+        }
 
   }
 
@@ -59,33 +69,61 @@ object ObservationModel extends ObservationOptics {
     implicit val DecoderCreate: Decoder[Create] =
       deriveDecoder[Create]
 
+    implicit val EqCreate: Eq[Create] =
+      Eq.by { a => (
+        a.observationId,
+        a.programId,
+        a.name,
+        a.asterismId,
+        a.status
+      )}
+
   }
 
   final case class Edit(
     observationId: Observation.Id,
-    existence:     Option[Existence],
-    name:          Option[Option[String]],
-    status:        Option[ObsStatus],
-    asterismId:    Option[Option[Asterism.Id]]
+    existence:     Input[Existence]   = Input.ignore,
+    name:          Input[String]      = Input.ignore,
+    status:        Input[ObsStatus]   = Input.ignore,
+    asterismId:    Input[Asterism.Id] = Input.ignore
   ) extends Editor[Observation.Id, ObservationModel] {
 
     override def id: Observation.Id =
       observationId
 
-    override def editor: ValidatedInput[State[ObservationModel, Unit]] =
-      (for {
-        _ <- ObservationModel.existence  := existence
-        _ <- ObservationModel.name       := name
-        _ <- ObservationModel.status     := status
-        _ <- ObservationModel.asterismId := asterismId
-      } yield ()).validNec
+    override def editor: ValidatedInput[State[ObservationModel, Unit]] = {
+      (existence.validateIsNotNull("existence"),
+       name     .validateNullable(n => ValidatedInput.nonEmptyString("name", n)),
+       status   .validateIsNotNull("status")
+      ).mapN { (e, n, s) =>
+        for {
+          _ <- ObservationModel.existence  := e
+          _ <- ObservationModel.name       := n
+          _ <- ObservationModel.status     := s
+          _ <- ObservationModel.asterismId := asterismId.toOptionOption
+        } yield ()
+      }
+    }
 
   }
 
   object Edit {
 
+    import io.circe.generic.extras.semiauto._
+    import io.circe.generic.extras.Configuration
+    implicit val customConfig: Configuration = Configuration.default.withDefaults
+
     implicit val DecoderEdit: Decoder[Edit] =
-      deriveDecoder[Edit]
+      deriveConfiguredDecoder[Edit]
+
+    implicit val EqEdit: Eq[Edit] =
+      Eq.by{ a => (
+        a.observationId,
+        a.existence,
+        a.name,
+        a.status,
+        a.asterismId
+      )}
 
   }
 
@@ -105,18 +143,18 @@ object ObservationModel extends ObservationOptics {
 trait ObservationOptics { self: ObservationModel.type =>
 
   val id: Lens[ObservationModel, Observation.Id] =
-    Lens[ObservationModel, Observation.Id](_.id)(a => b => b.copy(id = a))
+    Lens[ObservationModel, Observation.Id](_.id)(a => _.copy(id = a))
 
   val existence: Lens[ObservationModel, Existence] =
-    Lens[ObservationModel, Existence](_.existence)(a => b => b.copy(existence = a))
+    Lens[ObservationModel, Existence](_.existence)(a => _.copy(existence = a))
 
-  val name: Lens[ObservationModel, Option[String]] =
-    Lens[ObservationModel, Option[String]](_.name)(a => b => b.copy(name = a))
+  val name: Lens[ObservationModel, Option[NonEmptyString]] =
+    Lens[ObservationModel, Option[NonEmptyString]](_.name)(a => _.copy(name = a))
 
   val status: Lens[ObservationModel, ObsStatus] =
-    Lens[ObservationModel, ObsStatus](_.status)(a => b => b.copy(status = a))
+    Lens[ObservationModel, ObsStatus](_.status)(a => _.copy(status = a))
 
   val asterismId: Lens[ObservationModel, Option[Asterism.Id]] =
-    Lens[ObservationModel, Option[Asterism.Id]](_.asterismId)(a => b => b.copy(asterismId = a))
+    Lens[ObservationModel, Option[Asterism.Id]](_.asterismId)(a => _.copy(asterismId = a))
 
 }
