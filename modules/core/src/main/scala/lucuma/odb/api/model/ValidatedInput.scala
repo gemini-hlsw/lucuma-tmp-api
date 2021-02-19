87b61105618195e575780b08d495f447d2ae4cd1
Todd Burnside <tburnside@gemini.edu>
2021-02-19 09:17:13 -0800

    Change formatting in ValidatedInput

--- modules/core/src/main/scala/lucuma/odb/api/model/ValidatedInput.scala
+++ modules/core/src/main/scala/lucuma/odb/api/model/ValidatedInput.scala
@@ -18,21 +18,14 @@ object ValidatedInput {
     as.flattenOption match {
       case List(a) => a
       case Nil     => InputError.missingInput(name).invalidNec[A]
-      case _       =>
-        InputError.fromMessage(s"Multiple '$name' definitions are not permitted").invalidNec[A]
+      case _       => InputError.fromMessage(s"Multiple '$name' definitions are not permitted").invalidNec[A]
     }
 
-  def optionEither[A, B](
-    nameA: String,
-    nameB: String,
-    a:     Option[ValidatedInput[A]],
-    b:     Option[ValidatedInput[B]]
-  ): ValidatedInput[Option[Either[A, B]]] =
+  def optionEither[A, B](nameA: String, nameB: String, a: Option[ValidatedInput[A]], b: Option[ValidatedInput[B]]): ValidatedInput[Option[Either[A, B]]] =
     List(a.map(_.map(_.asLeft[B])), b.map(_.map(_.asRight[A]))).flattenOption match {
       case List(c) => c.map(_.some)
       case Nil     => Option.empty[Either[A, B]].validNec[InputError]
-      case _       =>
-        InputError.fromMessage(s"Either $nameA or $nameB are permitted but not both").invalidNec
+      case _       => InputError.fromMessage(s"Either $nameA or $nameB are permitted but not both").invalidNec
     }
 
   def nonEmptyString(name: String, s: String): ValidatedInput[NonEmptyString] =
@@ -41,8 +34,13 @@ object ValidatedInput {
       .leftMap(err => InputError.fromMessage(s"'$name' may not be empty: $err"))
       .toValidatedNec
 
-  def closedInterval[T](name: String, value: T, low: T, high: T)(implicit
-    v:                        Validate[T, Interval.Closed[low.type, high.type]]
+  def closedInterval[T](
+    name:  String, 
+    value: T, 
+    low:   T, 
+    high:  T
+  )(implicit
+    v:     Validate[T, Interval.Closed[low.type, high.type]]
   ): ValidatedInput[T Refined Interval.Closed[low.type, high.type]] =
     refineV[Interval.Closed[low.type, high.type]](value)
       .leftMap(_ => InputError.fromMessage(s"'$name' out of range: must be $low<= $name <= $high "))
