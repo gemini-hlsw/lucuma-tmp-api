25c50c6bef9a281b89ee85d9ee22be3316a38dc1
Shane Walker <swalker@gemini.edu>
2021-11-11 16:37:19 -0300

    Changes to accommodate lucuma-core 0.14.3

--- modules/core/src/main/scala/lucuma/odb/api/model/targetModel/TargetModel.scala
+++ modules/core/src/main/scala/lucuma/odb/api/model/targetModel/TargetModel.scala
@@ -3,13 +3,11 @@
 
 package lucuma.odb.api.model.targetModel
 
-import lucuma.core.`enum`.MagnitudeBand
-import lucuma.core.math.{Coordinates, Declination, Epoch, Parallax, ProperMotion, RadialVelocity, RightAscension}
-import lucuma.core.model.{CatalogId, Magnitude, SiderealTracking, Target}
+import lucuma.core.math.ProperMotion
+import lucuma.core.model.{SiderealTracking, Target}
 import cats.Eq
 import monocle.{Focus, Lens, Optional}
 
-import scala.collection.immutable.SortedMap
 
 /**
  * TargetModel pairs an id with a `lucuma.core.model.Target` and tracks the
@@ -39,31 +37,10 @@ trait TargetModelOptics { self: TargetModel.type =>
   val target: Lens[TargetModel, Target] =
     Focus[TargetModel](_.target)
 
-  val catalogId: Optional[Target, Option[CatalogId]] =
-    Target.siderealTracking.andThen(SiderealTracking.catalogId)
-
-  val coordinates: Optional[Target, Coordinates] =
-    Target.siderealTracking.andThen(SiderealTracking.baseCoordinates)
-
-  val ra: Optional[Target, RightAscension] =
-    coordinates.andThen(Coordinates.rightAscension)
-
-  val dec: Optional[Target, Declination] =
-    coordinates.andThen(Coordinates.declination)
-
-  val epoch: Optional[Target, Epoch] =
-    Target.siderealTracking.andThen(SiderealTracking.epoch)
+  // Target.properMotion is Optional[Target, ProperMotion], which makes it
+  // impossible to assign None to the ProperMotion of a SiderealTarget ?
 
   val properMotion: Optional[Target, Option[ProperMotion]] =
     Target.siderealTracking.andThen(SiderealTracking.properMotion)
-
-  val radialVelocity: Optional[Target, Option[RadialVelocity]] =
-    Target.siderealTracking.andThen(SiderealTracking.radialVelocity)
-
-  val parallax: Optional[Target, Option[Parallax]] =
-    Target.siderealTracking.andThen(SiderealTracking.parallax)
-
-  val magnitudes: Lens[Target, SortedMap[MagnitudeBand, Magnitude]] =
-    Target.magnitudes
 
 }
