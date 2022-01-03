7b700828250e0630b547481c9ff09c67c87294e4
Shane Walker <swalker@gemini.edu>
2022-01-03 14:07:45 -0300

    unnormalized sed input

---
+++ modules/core/src/main/scala/lucuma/odb/api/model/targetModel/SourceProfileSchemaModel.scala
@@ -0,0 +1,140 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.api.model.targetModel
+
+import cats.Eq
+import cats.data.{Nested, NonEmptyList, NonEmptyMap}
+import cats.Order.catsKernelOrderingForOrder
+import cats.syntax.either._
+import cats.syntax.functor._
+import cats.syntax.validated._
+import coulomb.Quantity
+import coulomb.si.Kelvin
+import eu.timepit.refined.cats._
+import eu.timepit.refined.types.all.PosBigDecimal
+import io.circe.Decoder
+import io.circe.generic.semiauto.deriveDecoder
+import io.circe.refined._
+import lucuma.core.`enum`.{CoolStarTemperature, GalaxySpectrum, HIIRegionSpectrum, PlanetSpectrum, PlanetaryNebulaSpectrum, QuasarSpectrum, StellarLibrarySpectrum}
+import lucuma.core.math.Wavelength
+import lucuma.core.model.UnnormalizedSED
+import lucuma.odb.api.model.{InputError, ValidatedInput, WavelengthModel}
+
+import scala.collection.immutable.SortedMap
+
+/**
+ *
+ */
+object SourceProfileSchemaModel {
+
+  final case class FluxDensityEntry(
+    wavelength: Wavelength,
+    density:    PosBigDecimal
+  )
+
+  object FluxDensityEntry {
+
+    def EqFluxDensityEntry: Eq[FluxDensityEntry] =
+      Eq.by { a => (
+        a.wavelength,
+        a.density
+      )}
+
+  }
+
+  final case class FluxDensityInput(
+    wavelength: WavelengthModel.Input,
+    density:    PosBigDecimal
+  ) {
+
+    val toFluxDensityEntry: ValidatedInput[FluxDensityEntry] =
+      wavelength.toWavelength("wavelength").map { w =>
+        FluxDensityEntry(w, density)
+      }
+
+  }
+
+  object FluxDensityInput {
+
+    implicit val DecoderFluxDensityInput: Decoder[FluxDensityInput] =
+      deriveDecoder[FluxDensityInput]
+
+    implicit val EqFluxDensityInput: Eq[FluxDensityInput] =
+      Eq.by { a => (
+        a.wavelength,
+        a.density
+      )}
+
+  }
+
+  final case class UnnormalizedSedInput(
+    stellarLibrary:  Option[StellarLibrarySpectrum],
+    coolStar:        Option[CoolStarTemperature],
+    galaxy:          Option[GalaxySpectrum],
+    planet:          Option[PlanetSpectrum],
+    quasar:          Option[QuasarSpectrum],
+    hiiRegion:       Option[HIIRegionSpectrum],
+    planetaryNebula: Option[PlanetaryNebulaSpectrum],
+    powerLaw:        Option[BigDecimal],
+    blackBodyTempK:  Option[PosBigDecimal],
+    fluxDensities:   Option[List[FluxDensityInput]]
+  ) {
+
+    val toUserDefined: Option[ValidatedInput[UnnormalizedSED.UserDefined]] =
+      fluxDensities.map { fluxDensityInputs =>
+        NonEmptyList
+          .fromList(fluxDensityInputs)
+          .toRight(InputError.fromMessage("One or more flux densities must be provided for a user defined SED"))
+          .toValidatedNec
+          .andThen(_.traverse(_.toFluxDensityEntry))
+          .map { nel =>
+            UnnormalizedSED.UserDefined(
+              NonEmptyMap(
+                nel.head.wavelength -> nel.head.density,
+                SortedMap.from {
+                  nel.tail.map { entry => entry.wavelength -> entry.density }
+                }
+              )
+            )
+          }
+      }
+
+
+    val toUnnormalizedSed: ValidatedInput[UnnormalizedSED] =
+      ValidatedInput.requireOne(
+        "sed",
+        Nested(List[Option[UnnormalizedSED]](
+          stellarLibrary .map(UnnormalizedSED.StellarLibrary(_)),
+          coolStar       .map(UnnormalizedSED.CoolStarModel(_)),
+          galaxy         .map(UnnormalizedSED.Galaxy(_)),
+          planet         .map(UnnormalizedSED.Planet(_)),
+          quasar         .map(UnnormalizedSED.Quasar(_)),
+          hiiRegion      .map(UnnormalizedSED.HIIRegion(_)),
+          planetaryNebula.map(UnnormalizedSED.PlanetaryNebula(_)),
+          powerLaw       .map(UnnormalizedSED.PowerLaw(_)),
+          blackBodyTempK .map(k => UnnormalizedSED.BlackBody(Quantity[PosBigDecimal, Kelvin](k))),
+        )).map(_.validNec[InputError]).value :+ toUserDefined
+      )
+  }
+
+  object UnnormalizedSedInput {
+
+    implicit val DecoderUnnormalizedSedInput: Decoder[UnnormalizedSedInput] =
+      deriveDecoder[UnnormalizedSedInput]
+
+    implicit val EqUnnormalizedSedInput: Eq[UnnormalizedSedInput] =
+      Eq.by { a => (
+        a.stellarLibrary,
+        a.coolStar,
+        a.galaxy,
+        a.planet,
+        a.quasar,
+        a.hiiRegion,
+        a.planetaryNebula,
+        a.powerLaw,
+        a.blackBodyTempK,
+        a.fluxDensities
+      )}
+  }
+}
