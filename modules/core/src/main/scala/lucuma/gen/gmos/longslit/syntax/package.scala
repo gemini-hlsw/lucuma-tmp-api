8002e67f5fbe85a10f2afe4f61d1b72b69ddab02
Shane Walker <swalker@gemini.edu>
2022-02-11 16:39:07 -0300

    GMOS longslit math

---
+++ modules/core/src/main/scala/lucuma/gen/gmos/longslit/syntax/package.scala
@@ -0,0 +1,13 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.gen.gmos.longslit
+
+package object syntax {
+
+  object all extends ToGmosNorthDisperserOps
+                with ToGmosSouthDisperserOps
+                with ToGmosNorthFpuOps
+                with ToGmosSouthFpuOps
+
+}
