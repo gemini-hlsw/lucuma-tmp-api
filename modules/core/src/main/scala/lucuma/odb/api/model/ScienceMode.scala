749a70e1d04ef92b7ba3d39e0d1fffe0ef77e040
Shane Walker <swalker@gemini.edu>
2022-04-25 17:39:54 -0400

    Refactor

--- modules/core/src/main/scala/lucuma/odb/api/model/ScienceMode.scala
+++ modules/core/src/main/scala/lucuma/odb/api/model/ScienceMode.scala
@@ -3,12 +3,16 @@
 
 package lucuma.odb.api.model
 
+import cats.Eq
 import coulomb.Quantity
+import coulomb.cats.implicits._
+import eu.timepit.refined.cats._
 import eu.timepit.refined.types.all.{NonEmptyString, PosDouble}
 import lucuma.core.`enum`.{GmosAmpGain, GmosAmpReadMode, GmosNorthDisperser, GmosNorthFilter, GmosNorthFpu, GmosRoi, GmosXBinning, GmosYBinning, ImageQuality, Instrument}
 import lucuma.core.math.{Angle, Offset}
 import lucuma.core.math.units.Nanometer
 import lucuma.core.model.SourceProfile
+import monocle.{Focus, Lens}
 
 sealed trait ScienceMode extends Product with Serializable {
 
@@ -17,6 +21,78 @@ sealed trait ScienceMode extends Product with Serializable {
 }
 
 object ScienceMode {
+
+  object GmosLongSlit {
+
+    sealed trait BasicConfig[G, F, U] {
+      def grating: G
+      def filter:  Option[F]
+      def fpu:     U
+    }
+
+    sealed trait AdvancedConfig[G, F, U] {
+      def name: Option[NonEmptyString]
+
+      def overrideBasic: BasicConfig[G, F, U]
+
+      def grating: G =
+        overrideBasic.grating
+
+      def filter: Option[F] =
+        overrideBasic.filter
+
+      def fpu: U =
+        overrideBasic.fpu
+
+      def explicitXBin: Option[GmosXBinning]
+
+      def explicitYBin: Option[GmosYBinning]
+
+      def yBin: GmosYBinning =
+        explicitYBin.getOrElse(AdvancedConfig.DefaultYBinning)
+
+      def explicitAmpReadMode: Option[GmosAmpReadMode]
+
+      def ampReadMode: GmosAmpReadMode =
+        explicitAmpReadMode.getOrElse(AdvancedConfig.DefaultAmpReadMode)
+
+      def explicitAmpGain: Option[GmosAmpGain]
+
+      def ampGain: GmosAmpGain =
+        explicitAmpGain.getOrElse(AdvancedConfig.DefaultAmpGain)
+
+      def explicitRoi: Option[GmosRoi]
+
+      def roi: GmosRoi =
+        explicitRoi.getOrElse(AdvancedConfig.DefaultRoi)
+
+      def explicitλDithers: Option[List[Quantity[Int, Nanometer]]]
+
+      def explicitSpatialOffsets: Option[List[Offset.Q]]
+
+      def spatialOffsets: List[Offset.Q] =
+        explicitSpatialOffsets.getOrElse(AdvancedConfig.DefaultSpatialOffsets)
+
+    }
+
+    object AdvancedConfig {
+      val DefaultYBinning: GmosYBinning =
+        GmosYBinning.Two
+
+      val DefaultAmpReadMode: GmosAmpReadMode =
+        GmosAmpReadMode.Slow
+
+      val DefaultAmpGain: GmosAmpGain =
+        GmosAmpGain.Low
+
+      val DefaultRoi: GmosRoi =
+        GmosRoi.FullFrame
+
+      val DefaultSpatialOffsets: List[Offset.Q] =
+        List(Offset.Q.Zero, Offset.Q(Angle.arcseconds.reverseGet(15)))
+    }
+
+  }
 
   /**
    * GmosNorthLongSlit mode. BasicConfig options can be overridden or expanded
@@ -42,16 +118,40 @@ object ScienceMode {
      * information in the original BasicConfig.
      */
     final case class BasicConfig(
-      grating:   GmosNorthDisperser,
-      filter:    Option[GmosNorthFilter],
-      fpu:       GmosNorthFpu
-    ) {
+      grating: GmosNorthDisperser,
+      filter:  Option[GmosNorthFilter],
+      fpu:     GmosNorthFpu
+    ) extends GmosLongSlit.BasicConfig[GmosNorthDisperser, GmosNorthFilter, GmosNorthFpu] {
 
       def toAdvanced: AdvancedConfig =
         AdvancedConfig(
           name          = None,
           overrideBasic = this
         )
+
+    }
+
+    object BasicConfig extends BasicConfigOptics {
+
+      implicit val EqBasicConfig: Eq[BasicConfig] =
+        Eq.by { a => (
+          a.grating,
+          a.filter,
+          a.fpu
+        )}
+
+    }
+
+    sealed trait BasicConfigOptics { self: BasicConfig.type =>
+
+      val grating: Lens[BasicConfig, GmosNorthDisperser] =
+        Focus[BasicConfig](_.grating)
+
+      val filter: Lens[BasicConfig, Option[GmosNorthFilter]] =
+        Focus[BasicConfig](_.filter)
+
+      val fpu: Lens[BasicConfig, GmosNorthFpu] =
+        Focus[BasicConfig](_.fpu)
 
     }
 
@@ -69,34 +169,13 @@ object ScienceMode {
       explicitRoi:            Option[GmosRoi]                        = None,
       explicitλDithers:       Option[List[Quantity[Int, Nanometer]]] = None,
       explicitSpatialOffsets: Option[List[Offset.Q]]                 = None
-    ) {
+    ) extends GmosLongSlit.AdvancedConfig[GmosNorthDisperser, GmosNorthFilter, GmosNorthFpu] {
 
       import lucuma.gen.gmos.longslit.syntax.gmosNorthGrating._
       import lucuma.gen.gmos.longslit.syntax.gmosNorthFpu._
 
-      def grating: GmosNorthDisperser =
-        overrideBasic.grating
-
-      def filter: Option[GmosNorthFilter] =
-        overrideBasic.filter
-
-      def fpu: GmosNorthFpu =
-        overrideBasic.fpu
-
       def xBin(sourceProfile: SourceProfile, imageQuality: ImageQuality, sampling: PosDouble): GmosXBinning =
         explicitXBin.getOrElse(fpu.xbin(sourceProfile, imageQuality, sampling))
-
-      def yBin: GmosYBinning =
-        explicitYBin.getOrElse(AdvancedConfig.DefaultYBinning)
-
-      def ampReadMode: GmosAmpReadMode =
-        explicitAmpReadMode.getOrElse(AdvancedConfig.DefaultAmpReadMode)
-
-      def ampGain: GmosAmpGain =
-        explicitAmpGain.getOrElse(AdvancedConfig.DefaultAmpGain)
-
-      def roi: GmosRoi =
-        explicitRoi.getOrElse(AdvancedConfig.DefaultRoi)
 
       def λDithers: List[Quantity[Int, Nanometer]] =
         explicitλDithers.getOrElse {
@@ -106,27 +185,62 @@ object ScienceMode {
           )
         }
 
-      def spatialOffsets: List[Offset.Q] =
-        explicitSpatialOffsets.getOrElse(AdvancedConfig.DefaultSpatialOffsets)
+    }
+
+    object AdvancedConfig extends AdvancedConfigOptics {
+
+      implicit val EqAdvancedConfig: Eq[AdvancedConfig] =
+        Eq.by { a => (
+          a.name,
+          a.overrideBasic,
+          a.explicitXBin,
+          a.explicitYBin,
+          a.explicitAmpReadMode,
+          a.explicitAmpGain,
+          a.explicitRoi,
+          a.explicitλDithers,
+          a.explicitSpatialOffsets
+        )}
 
     }
 
-    object AdvancedConfig {
+    sealed trait AdvancedConfigOptics { self: AdvancedConfig.type =>
 
-      val DefaultYBinning: GmosYBinning =
-        GmosYBinning.Two
+      val name: Lens[AdvancedConfig, Option[NonEmptyString]] =
+        Focus[AdvancedConfig](_.name)
 
-      val DefaultAmpReadMode: GmosAmpReadMode =
-        GmosAmpReadMode.Slow
+      val overrideBasic: Lens[AdvancedConfig, BasicConfig] =
+        Focus[AdvancedConfig](_.overrideBasic)
 
-      val DefaultAmpGain: GmosAmpGain =
-        GmosAmpGain.Low
+      val grating: Lens[AdvancedConfig, GmosNorthDisperser] =
+        overrideBasic.andThen(BasicConfig.grating)
 
-      val DefaultRoi: GmosRoi =
-        GmosRoi.FullFrame
+      val filter: Lens[AdvancedConfig, Option[GmosNorthFilter]] =
+        overrideBasic.andThen(BasicConfig.filter)
 
-      val DefaultSpatialOffsets: List[Offset.Q] =
-        List(Offset.Q.Zero, Offset.Q(Angle.arcseconds.reverseGet(15)))
+      val fpu: Lens[AdvancedConfig, GmosNorthFpu] =
+        overrideBasic.andThen(BasicConfig.fpu)
+
+      val explicitXBin: Lens[AdvancedConfig, Option[GmosXBinning]] =
+        Focus[AdvancedConfig](_.explicitXBin)
+
+      val explicitYBin: Lens[AdvancedConfig, Option[GmosYBinning]] =
+        Focus[AdvancedConfig](_.explicitYBin)
+
+      val explicitAmpReadMode: Lens[AdvancedConfig, Option[GmosAmpReadMode]] =
+        Focus[AdvancedConfig](_.explicitAmpReadMode)
+
+      val explicitAmpGain: Lens[AdvancedConfig, Option[GmosAmpGain]] =
+        Focus[AdvancedConfig](_.explicitAmpGain)
+
+      val explicitRoi: Lens[AdvancedConfig, Option[GmosRoi]] =
+        Focus[AdvancedConfig](_.explicitRoi)
+
+      val explicitλDithers: Lens[AdvancedConfig, Option[List[Quantity[Int, Nanometer]]]] =
+        Focus[AdvancedConfig](_.explicitλDithers)
+
+      val explicitSpatialOffsets: Lens[AdvancedConfig, Option[List[Offset.Q]]] =
+        Focus[AdvancedConfig](_.explicitSpatialOffsets)
 
     }
 
